098dc9c6673c237559fa197dbd013efee15b1c6b
bliessens <benoni29@gmail.com>
2018-09-12 11:13:48 +0200

    cleanup gradle

--- src/test/resources/deploy-zk-cluster.sh
+++ src/test/resources/deploy-zk-cluster.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-docker network create --internal --driver bridge backbone
+ZK_VERSION=3.4.13
+NETWORK=backbone
 
-docker run -d --name zookeeper1 --network backbone -e ZOO_MY_ID=1 -e ZOO_SERVERS="server.1=0.0.0.0:2888:3888 server.2=zookeeper2:2888:3888 server.3=zookeeper3:2888:3888" zookeeper:3.4.13
-docker run -d --name zookeeper2 --network backbone -e ZOO_MY_ID=2 -e ZOO_SERVERS="server.1=zookeeper1:2888:3888 server.2=0.0.0.0:2888:3888 server.3=zookeeper3:2888:3888" zookeeper:3.4.13
-docker run -d --name zookeeper3 --network backbone -e ZOO_MY_ID=3 -e ZOO_SERVERS="server.1=zookeeper1:2888:3888 server.2=zookeeper2:2888:3888 server.3=0.0.0.0:2888:3888" zookeeper:3.4.13
+docker network create --internal --driver bridge $NETWORK
+
+docker run -d --name zookeeper1 --network $NETWORK -e ZOO_MY_ID=1 -e ZOO_SERVERS="server.1=0.0.0.0:2888:3888 server.2=zookeeper2:2888:3888 server.3=zookeeper3:2888:3888" zookeeper:$ZK_VERSION
+docker run -d --name zookeeper2 --network $NETWORK -e ZOO_MY_ID=2 -e ZOO_SERVERS="server.1=zookeeper1:2888:3888 server.2=0.0.0.0:2888:3888 server.3=zookeeper3:2888:3888" zookeeper:$ZK_VERSION
+docker run -d --name zookeeper3 --network $NETWORK -e ZOO_MY_ID=3 -e ZOO_SERVERS="server.1=zookeeper1:2888:3888 server.2=zookeeper2:2888:3888 server.3=0.0.0.0:2888:3888" zookeeper:$ZK_VERSION
