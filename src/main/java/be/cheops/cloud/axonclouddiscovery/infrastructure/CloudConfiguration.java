b5e5146b83e7d32d3b15cff470c296660c96e859
bliessens <benoni29@gmail.com>
2018-09-16 20:59:45 +0200

    Zookeeper doesn't support ServiceInstance.Metadata -> switch to SpringCloudHttpBackupCommandRouter which polls target service for supported commands

--- src/main/java/be/cheops/cloud/axonclouddiscovery/infrastructure/CloudConfiguration.java
+++ src/main/java/be/cheops/cloud/axonclouddiscovery/infrastructure/CloudConfiguration.java
@@ -2,18 +2,13 @@ package be.cheops.cloud.axonclouddiscovery.infrastructure;
 
 import com.thoughtworks.xstream.XStream;
 import org.axonframework.commandhandling.CommandBus;
-import org.axonframework.commandhandling.SimpleCommandBus;
-import org.axonframework.commandhandling.distributed.AnnotationRoutingStrategy;
-import org.axonframework.commandhandling.distributed.CommandBusConnector;
-import org.axonframework.commandhandling.distributed.CommandRouter;
-import org.axonframework.commandhandling.distributed.DistributedCommandBus;
-import org.axonframework.common.transaction.TransactionManager;
-import org.axonframework.monitoring.NoOpMessageMonitor;
+import org.axonframework.commandhandling.distributed.*;
 import org.axonframework.serialization.xml.CompactDriver;
 import org.axonframework.serialization.xml.XStreamSerializer;
-import org.axonframework.springcloud.commandhandling.SpringCloudCommandRouter;
+import org.axonframework.springcloud.commandhandling.SpringCloudHttpBackupCommandRouter;
 import org.axonframework.springcloud.commandhandling.SpringHttpCommandBusConnector;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
@@ -21,23 +16,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @Configuration
 @Profile({"!single", "cloud"})
 class CloudConfiguration {
 
+    private static final RoutingStrategy ROUTING_STRATEGY = new AnnotationRoutingStrategy();
+
+//    @Bean
+//    public CommandRouter springCloudCommandRouter(DiscoveryClient discoveryClient) {
+//        return new SpringCloudCommandRouter(discoveryClient, ROUTING_STRATEGY);
+//    }
+
     @Bean
-    public CommandRouter springCloudCommandRouter(DiscoveryClient discoveryClient) {
-        return new SpringCloudCommandRouter(discoveryClient, new AnnotationRoutingStrategy());
+    public CommandRouter springCloudHttpBackupCommandRouter(DiscoveryClient discoveryClient,
+                                                            RestTemplate restTemplate,
+                                                            @Value("${axon.distributed.spring-cloud.fallback-url}") String messageRoutingInformationEndpoint) {
+
+        return new SpringCloudHttpBackupCommandRouter(discoveryClient, ROUTING_STRATEGY, restTemplate, messageRoutingInformationEndpoint);
     }
 
     @Bean
-    public CommandBusConnector springHttpCommandBusConnector(@Qualifier("localCommandBus") CommandBus localSegment,
-                                                             RestOperations restOperations) {
+    public CommandBusConnector springHttpCommandBusConnector(@Qualifier("localSegment") CommandBus localSegment,
+                                                             RestOperations restTemplate) {
         final CommandSerializerFactory serializerFactory = new CommandSerializerFactory();
 
-        return new SpringHttpCommandBusConnector(localSegment, restOperations, new XStreamSerializer(serializerFactory.newXStream()));
+        return new SpringHttpCommandBusConnector(localSegment, restTemplate, new XStreamSerializer(serializerFactory.newXStream()));
     }
 
     @Bean("distributedCommandBus")
@@ -47,10 +53,10 @@ class CloudConfiguration {
         return new DistributedCommandBus(commandRouter, commandBusConnector);
     }
 
-    @Bean("localCommandBus")
-    public SimpleCommandBus commandBus(TransactionManager axonTransactionManager) {
-        return new SimpleCommandBus(axonTransactionManager, NoOpMessageMonitor.INSTANCE);
-    }
+//    @Bean({"localCommandBus", "localSegment"})
+//    public SimpleCommandBus commandBus(TransactionManager axonTransactionManager) {
+//        return new SimpleCommandBus(axonTransactionManager, NoOpMessageMonitor.INSTANCE);
+//    }
 
     static class CommandSerializerFactory {
         XStream newXStream() {
