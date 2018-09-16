ffffc873e6b44d045e060292d292d2a99bde21f2
bliessens <benoni29@gmail.com>
2018-09-16 21:08:34 +0200

    Axon auto config provides SimpleCommandBus

--- src/main/java/be/cheops/cloud/axonclouddiscovery/infrastructure/EventStoreConfiguration.java
+++ src/main/java/be/cheops/cloud/axonclouddiscovery/infrastructure/EventStoreConfiguration.java
@@ -18,7 +18,6 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 
-//@EnableAxon
 @Configuration
 //@EnableScheduling
 //@EnableTransactionManagement
--- src/main/java/be/cheops/cloud/axonclouddiscovery/infrastructure/SingleNodeConfiguration.java
+++ src/main/java/be/cheops/cloud/axonclouddiscovery/infrastructure/SingleNodeConfiguration.java
@@ -1,9 +1,5 @@
 package be.cheops.cloud.axonclouddiscovery.infrastructure;
 
-import org.axonframework.commandhandling.SimpleCommandBus;
-import org.axonframework.common.transaction.TransactionManager;
-import org.axonframework.monitoring.NoOpMessageMonitor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -11,9 +7,9 @@ import org.springframework.context.annotation.Profile;
 @Profile({"single"})
 class SingleNodeConfiguration {
 
-    @Bean("commandBus")
-    public SimpleCommandBus commandBus(TransactionManager axonTransactionManager) {
-        return new SimpleCommandBus(axonTransactionManager, NoOpMessageMonitor.INSTANCE);
-    }
+//    @Bean("commandBus")
+//    public SimpleCommandBus commandBus(TransactionManager axonTransactionManager) {
+//        return new SimpleCommandBus(axonTransactionManager, NoOpMessageMonitor.INSTANCE);
+//    }
 
 }
